ae30894ed3da03f5d60e5b2f0c950ac66ef4a339
Alex S <olstakh@microsoft.com>
2026-02-19 17:03:09 -0800

    Initial

---
+++ src/Olstakh.CodeAnalysisMonitor/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace Olstakh.CodeAnalysisMonitor;
+
+/// <summary>
+/// This is a placeholder class in the Olstakh.CodeAnalysisMonitor project. It serves as an example of a class that would be part of the assembly being analyzed by the Olstakh.CodeAnalysisMonitorAnalyzer. The presence of this class allows the analyzer to check the assembly name and report diagnostics if it is still named "Olstakh.CodeAnalysisMonitor". In a real project, this class would contain actual implementation code relevant to the project's functionality.
+/// </summary>
+public class Class1
+{
+
+}
